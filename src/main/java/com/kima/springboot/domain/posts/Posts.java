205aae4bbab5fd4b034ce52d87c0999382734e45
Kim-AYoung <onionlily123@naver.com>
2022-06-05 15:36:18 +0900

    using jpa

---
+++ src/main/java/com/kima/springboot/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.kima.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity // 실제 DB의 테이블과 매칭될 클래스
+public class Posts {
+
+    @Id // PK
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙, IDENTITY : auto_increment
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/kima/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.kima.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> { // 기본적인 CRUD 메소드가 자동 생성
+
+
+}
