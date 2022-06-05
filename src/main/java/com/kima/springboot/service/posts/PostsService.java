3e455cf9f743efb69e2aa1b9f0deec1bfd134f59
Kim-AYoung <onionlily123@naver.com>
2022-06-05 18:14:29 +0900

    [community web application] add update/get posts features

--- src/main/java/com/kima/springboot/service/posts/PostsService.java
+++ src/main/java/com/kima/springboot/service/posts/PostsService.java
@@ -1,10 +1,15 @@
 package com.kima.springboot.service.posts;
 
+import com.kima.springboot.domain.posts.Posts;
 import com.kima.springboot.domain.posts.PostsRepository;
+import com.kima.springboot.web.dto.PostsResponseDto;
 import com.kima.springboot.web.dto.PostsSaveRequestDto;
+import com.kima.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +20,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent()); // dirty checking : 영속성 컨텍스트가 유지된 상태이므로 해당 데이터의 값을 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 변경분을 반영
+        return id;
     }
 }
