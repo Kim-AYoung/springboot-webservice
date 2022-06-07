d51ce5511b7de049e742b1965def02d86e53a859
Kim-AYoung <onionlily123@naver.com>
2022-06-07 16:30:02 +0900

    [community web application] add UI for looking up posts

--- src/main/java/com/kima/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/kima/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,13 @@
 package com.kima.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> { // 기본적인 CRUD 메소드가 자동 생성
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC") // Spring Data JPA에서 제공하지 않는 메소드는 이와 같이 쿼리로 작성, Entity 클래스만으로 처리하기 어려운 복잡한 조회를 할 때는 보통 spring data jpa 대신 `querydsl`이라는 프레임워크로 사용
+    List<Posts> findAllDesc();
 
 }
--- src/main/java/com/kima/springboot/service/posts/PostsService.java
+++ src/main/java/com/kima/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.kima.springboot.service.posts;
 
 import com.kima.springboot.domain.posts.Posts;
 import com.kima.springboot.domain.posts.PostsRepository;
+import com.kima.springboot.web.dto.PostsListResponseDto;
 import com.kima.springboot.web.dto.PostsResponseDto;
 import com.kima.springboot.web.dto.PostsSaveRequestDto;
 import com.kima.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +34,12 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent()); // dirty checking : 영속성 컨텍스트가 유지된 상태이므로 해당 데이터의 값을 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 변경분을 반영
         return id;
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선됨
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // 람다식, 다음과 뜻이 같음 .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList()); // List로 반환
     }
 }
