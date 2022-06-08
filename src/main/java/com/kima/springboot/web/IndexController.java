8e4628d898e68c8f272e9266046664391131be59
Kim-AYoung <onionlily123@naver.com>
2022-06-08 21:46:49 +0900

    [community web application] refactoring by using annotation. Because the process of getting the session value is duplicated on multiple controllers, the duplication is eliminated through HandlerMethodArgumentResolver (Annotation method).

--- src/main/java/com/kima/springboot/web/IndexController.java
+++ src/main/java/com/kima/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kima.springboot.web;
 
+import com.kima.springboot.config.auth.LoginUser;
 import com.kima.springboot.config.auth.dto.SessionUser;
 import com.kima.springboot.service.posts.PostsService;
 import com.kima.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
