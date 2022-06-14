866018290237031bcb365e7f615b4f00b14f827c
Kim-AYoung <onionlily123@naver.com>
2022-06-14 23:26:45 +0900

    [community web application] add Profile API

---
+++ src/test/java/com/kima/springboot/web/ProfileControllerUnitTest.java
@@ -0,0 +1,57 @@
+package com.kima.springboot.web;
+
+import org.junit.Test;
+import org.assertj.core.api.Assertions;
+import org.springframework.mock.env.MockEnvironment;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void getRealProfile() {
+        //given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        Assertions.assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void getFirstIfNotExistRealProfile() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        Assertions.assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void getDefaultIfNotExistActiveProfile() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        Assertions.assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
